184566f36e347c983b0245bbfad14952cfb5b828
rfranett <rfranett@gmail.com>
2023-11-24 16:49:28 -0300

    Datatimemc#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/*ex1
+
+DateTime dataAtual = DateTime.Now.AddDays(7);
+Console.WriteLine(dataAtual);
+*/
+
+//ex3
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/mm/yyyy HH:mm:ss"));
+
+/*ex2
+
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/mm/yyyy"));
+*/
+
+
---
+++ models/TipoDataTime.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project.net4.models
+{
+    public class TipoDataTimes
+    {
+        
+    }
+}
